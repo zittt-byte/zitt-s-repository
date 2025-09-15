a3d2af5eb020a5973e3dbd4228aa7946167259cf
Yasit <118095658+zittt-byte@users.noreply.github.com>
2025-09-15 11:56:38 +0700

    Update helloworld.py
    change variable

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,5 @@
 '''test for hello world'''
-def print_text (word,time):
-    for _ in range(time) :
+def print_text (word,times):
+    for _ in range(times) :
         print(word)
 print_text(str(input()),int(input()))
